ef6f09cea58b75a92c234a51cdbfe9c7776532f6
Sarah GP <sarah.groff.palermo@gmail.com>
2015-08-03 12:53:07 -0400

    Parses one file, with functions nested one deep

--- examples/whenever.js/whenever.js
+++ examples/whenever.js/whenever.js
@@ -1,1 +1,25 @@
-whenever.js
+var _     = require('lodash'),
+    fs    = require('fs'),
+    path  = require('path'),
+    PEG   = require('pegjs'),
+    cli  = require('commander');
+
+cli
+  .version('0.0.1')
+  .description('Compile your whenever files by passing the path to the file')
+  .parse(process.argv);
+
+// Find the files
+var file = fs.readFileSync(cli.args[0]);
+
+
+
+// Use the grammar to make a parser
+
+var grammar = fs.readFileSync(__dirname + '/lib/grammar.txt').toString(),
+    parser  = PEG.buildParser(grammar),
+    bag     = parser.parse(file.toString());
+
+console.log(bag);
+
+// Parse the files we found and send them to bag file ... run bag file
